d8d135d9f524d1266e5052073453d5d893ddeafb
ponbac <root@ponbac.xyz>
2024-05-18 14:48:39 +0200

    queues

--- src/commands/setup.rs
+++ src/commands/setup.rs
@@ -5,4 +5,16 @@ use crate::git::init_submodules;
 pub(crate) fn invoke(root_path: &Path) {
     println!("Running setup command");
     init_submodules(root_path);
+
+    // execute ELOSQueues.ps1
+    let queues_status = std::process::Command::new("powershell")
+        .args([
+            "-File",
+            root_path
+                .join("ELOSQueues.ps1")
+                .to_str()
+                .expect("ELOSQueues.ps1 path is not valid"),
+        ])
+        .status()
+        .expect("Failed to run ELOSQueues.ps1");
 }
