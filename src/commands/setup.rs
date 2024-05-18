44d7647e01495f145ea8cf142297905d521c8369
ponbac <root@ponbac.xyz>
2024-05-18 15:13:49 +0200

    account param

--- src/commands/setup.rs
+++ src/commands/setup.rs
@@ -6,7 +6,8 @@ pub(crate) fn invoke(root_path: &Path) {
     println!("Running setup command");
     init_submodules(root_path);
 
-    // execute ELOSQueues.ps1
+    // execute ELOSQueues.ps1 with argument -account <account_name>
+    let account_name = "pbac@spinit.local";
     let queues_status = std::process::Command::new("powershell")
         .args([
             "-File",
@@ -14,6 +15,8 @@ pub(crate) fn invoke(root_path: &Path) {
                 .join("ELOSQueues.ps1")
                 .to_str()
                 .expect("ELOSQueues.ps1 path is not valid"),
+            "-account",
+            account_name,
         ])
         .status()
         .expect("Failed to run ELOSQueues.ps1");
